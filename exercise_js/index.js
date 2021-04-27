60c9be42f0ec0f4ba3a1caec36901f6a1f08faf3
st-baonguyen <bao.nguyen@supremetech.vn>
2021-04-27 09:24:57 +0700

    restructuring

--- exercise_js/index.js
+++ exercise_js/index.js
@@ -1,34 +1,29 @@
+// 1. Write a JavaScript program to compute the sum of the two given integers.
+// If the two values are same, then returns triple their sum.
 function sum(a, b) {
   return a === b ? (a + b) * 3 : a + b;
 }
+
+// 2. Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19.
 
 function absolute(a) {
   return Math.abs(a - 19) > 19 ? Math.abs(a - 19) * 3 : Math.abs(a - 19);
 }
 
-function maskedNumber3(a) {
+function maskedNumber(a) {
+  const variableOne = 3;
+  const variableTwo = 6;
   let temp = a.split("");
   let index = temp.indexOf("*");
-  let arr = [];
+  let arrOne = [];
+  let arrTwo = [];
   for (let i = 0; i <= 9; i++) {
     let item = temp
       .slice(0, index)
       .concat(i)
       .concat(temp.slice(index + 1));
-    if (item.join("") % 3 === 0) arr.push(item.join(""));
+    if (item.join("") % variableOne === 0) arrOne.push(item.join(""));
+    if (item.join("") % variableTwo === 0) arrTwo.push(item.join(""));
   }
   console.log(arr);
-}
-
-function maskedNumber6(a) {
-  let temp = a.split("");
-  let index = temp.indexOf("*");
-  let arr = [];
-  for (let i = 0; i <= 9; i++) {
-    let item = temp
-      .slice(0, index)
-      .concat(i)
-      .concat(temp.slice(index + 1));
-    if (item.join("") % 6 === 0) arr.push(item.join(""));
-  }
 }
