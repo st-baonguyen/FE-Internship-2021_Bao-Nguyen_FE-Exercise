1ce3c3a8500a548571c9a1ee217b5bdfd243a027
st-baonguyen <bao.nguyen@supremetech.vn>
2021-04-27 09:45:55 +0700

    change variable

--- exercise_js/index.js
+++ exercise_js/index.js
@@ -1,14 +1,19 @@
 // 1. Write a JavaScript program to compute the sum of the two given integers.
 // If the two values are same, then returns triple their sum.
 function sum(a, b) {
-  return a === b ? (a + b) * 3 : a + b;
+  const variable = 3;
+  return a === b ? (a + b) * variable : a + b;
 }
 
 // 2. Write a JavaScript program to compute the absolute difference between a specified number and 19.
 // Returns triple their absolute difference if the specified number is greater than 19.
 
 function absolute(a) {
-  return Math.abs(a - 19) > 19 ? Math.abs(a - 19) * 3 : Math.abs(a - 19);
+  const variable = 19;
+  const divisor = 3;
+  return Math.abs(a - variable) > variable
+    ? Math.abs(a - variable) * divisor
+    : Math.abs(a - variable);
 }
 
 // 3+4. A masked number is a string that consists of digits and one asterisk (*) that should be replaced by exactly one digit.
