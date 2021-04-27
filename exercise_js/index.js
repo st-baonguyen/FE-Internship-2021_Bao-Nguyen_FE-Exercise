407693be7078f215b6f6b838eac8e0f758c65c79
st-baonguyen <bao.nguyen@supremetech.vn>
2021-04-27 09:29:57 +0700

    restructuring

--- exercise_js/index.js
+++ exercise_js/index.js
@@ -4,11 +4,15 @@ function sum(a, b) {
   return a === b ? (a + b) * 3 : a + b;
 }
 
-// 2. Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19.
+// 2. Write a JavaScript program to compute the absolute difference between a specified number and 19.
+// Returns triple their absolute difference if the specified number is greater than 19.
 
 function absolute(a) {
   return Math.abs(a - 19) > 19 ? Math.abs(a - 19) * 3 : Math.abs(a - 19);
 }
+
+// 3+4. A masked number is a string that consists of digits and one asterisk (*) that should be replaced by exactly one digit.
+// Given a masked number find all the possible options to replace the asterisk with a digit to produce an integer divisible by 3 and by 6.
 
 function maskedNumber(a) {
   const variableOne = 3;
@@ -25,5 +29,6 @@ function maskedNumber(a) {
     if (item.join("") % variableOne === 0) arrOne.push(item.join(""));
     if (item.join("") % variableTwo === 0) arrTwo.push(item.join(""));
   }
-  console.log(arr);
+  console.log(arrOne);
+  console.log(arrTwo);
 }
