a7797fd0635f5cd5fffe45f4db52d16ed0806af3
st-baonguyen <bao.nguyen@supremetech.vn>
2021-05-04 15:20:35 +0700

    fix empty cart

--- excercise-e-shop/scripts/cart.js
+++ excercise-e-shop/scripts/cart.js
@@ -1,68 +1,4 @@
-const products = [
-  {
-    id: 1,
-    name: "T-Shirt Summer Vibes",
-    price: "$89.99",
-    realPrice: "$119.99",
-    amount: 1,
-    image: "./asset/sample/product4.png",
-  },
-  {
-    id: 2,
-    name: "Loose Knit 3/4 Sleeve",
-    price: "$119.99",
-    amount: 1,
-    image: "./asset/sample/product5.png",
-  },
-  {
-    id: 3,
-    name: "Basic Slim Fit T-Shirt",
-    price: "$79.99",
-    amount: 1,
-    image: "./asset/sample/product7.png",
-  },
-  {
-    id: 4,
-    name: "Loose Textured T-Shirt",
-    price: "$119.99",
-    amount: 1,
-    image: "../asset/sample/product8.png",
-  },
-  {
-    id: 1,
-    name: "T-Shirt Summer Vibes",
-    price: "$89.99",
-    realPrice: "$119.99",
-    amount: 1,
-    image: "../asset/sample/product4.png",
-  },
-  {
-    id: 2,
-    name: "Loose Knit 3/4 Sleeve",
-    price: "$119.99",
-    amount: 1,
-    image: "../asset/sample/product5.png",
-  },
-  {
-    id: 3,
-    name: "Basic Slim Fit T-Shirt",
-    price: "$79.99",
-    amount: 1,
-    image: "../asset/sample/product7.png",
-  },
-  {
-    id: 4,
-    name: "Loose Textured T-Shirt",
-    price: "$119.99",
-    amount: 1,
-    image: "../asset/sample/product8.png",
-  },
-];
-
-function formartMoney(money) {
-  money.slice(1);
-}
-
+// render product
 function renderItem(item) {
   return `
   <li class="prd-item">
@@ -73,25 +9,24 @@ function renderItem(item) {
           class="prd-img"
           alt="T-Shirt Summer Vibes"
         />
-        <h4 class="prd-name">${item.name}</h4>
-      </div>
-      <div class="prd-body-right">
-        <div class="price">
-          <span class="prd-price">${item.price}</span>
-          <div class="badge-sell">
-            <span class="real-price-off">${
-              item.realPrice ? item.realPrice : ""
-            }</span>
+        <div class="prd-info flex-between-y">
+          <h4 class="prd-name">${item.name}</h4>
+          <span class="prd-price">${(item.percent) !== 0 ? "$"+(item.price -item.price*item.percent/100).toFixed(2) : "$"+item.price}</span>
+        </div>        
+      </div>  
+      <div class="prd-body-right flex-between-y">
+        <div class="total">
+          <span class="prd-total">${((item.percent) !== 0 ?"$"+(item.price -item.price*item.percent/100): "$"+(item.price * item.amount).toFixed(2))}</span>
+          <div class="badge-sell flex-center-x">
             <span class="percent-sell-off">${
-              item.realPrice
-                ? 100 - +item.price.slice(1) / +item.realPrice.slice(1) + "%"
-                : ""
+              item.percent !== 0 ? '-'+ item.percent + '%' : ''
             }</span>
+            <span class="real-price">${item.percent !== 0 ? item.price : ''}</span>
           </div>
         </div>
         <div class="prd-amount">
           <span class="amount decrese-amount">-</span>
-          <span class="amount-now">${item.amount}</span>
+          <input class="amount-now" value=${item.amount} onkeyup={changeQualityInput(value)}>
           <span class="amount increse-amount">+</span>
         </div>
         <span class="remove">Remove</span>
@@ -100,76 +35,41 @@ function renderItem(item) {
   </li>
   `;
 }
-// show number at cart icon
-const cartNumber = localStorage.getItem("cartNumber");
-const numCart = document.querySelector(".cart");
-numCart.setAttribute("data-cart", +cartNumber);
 
-let carts = document.querySelectorAll(".btn-add-cart");
-
-for (let i = 0; i < carts.length; i++) {
-  carts[i].addEventListener("click", function () {
-    addToLocal(products[i]);
-  });
-}
+// function general for cart page
 function getItem() {
   return JSON.parse(localStorage.getItem("listItem"));
 }
 
+// default function
 displayCart();
-function addToLocal(product) {
-  const cartNumber = localStorage.getItem("cartNumber");
-  localStorage.setItem("cartNumber", +cartNumber + 1 || 1);
-  const numCart = document.querySelector(".cart");
-  numCart.setAttribute("data-cart", +cartNumber + 1);
-  setItem(product);
-}
 
+//set number at cart icon when change quality 
 function addNumberCart() {
   let listPrd = getItem();
-  let num = 0;
+  let number = 0;
   listPrd.forEach((element) => {
-    num += element.amount;
+    number += element.amount;
   });
-  localStorage.setItem("cartNumber", num);
+  localStorage.setItem("cartNumber", number);
   const cartNumber = localStorage.getItem("cartNumber");
   const numCart = document.querySelector(".cart");
   numCart.setAttribute("data-cart", cartNumber);
 }
 
-function setItem(product) {
-  let prd = getItem();
-  let arr = prd || [];
-  if (arr.length === 0) {
-    arr.push(product);
-  } else {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].id == product.id) {
-        arr[i].amount += 1;
-      } else {
-        count++;
-      }
-    }
-    if (count === arr.length) {
-      arr.push(product);
-    }
-  }
-  localStorage.setItem("listItem", JSON.stringify(arr));
-  addNumberCart();
-  totalCost();
-}
-
+//total money
 function totalCost() {
   let price = getItem();
   let total = 0;
   for (const key in price) {
-    total += +price[key].price.slice(1) * +price[key].amount;
+    total += +price[key].price * +price[key].amount;
   }
-  if (total)
+  if (total) {
     document.querySelector(".sub-price").innerHTML = "$" + total.toFixed(2);
+  }
 }
 
+// render product list and show int cart
 function displayCart() {
   let productItem = getItem();
   let listPrd = "";
@@ -180,14 +80,38 @@ function displayCart() {
     document.querySelector(".prd-list").innerHTML = listPrd;
   }
   totalCost();
+  addNumberCart();
+  removeItems();
+  amountItem("decrese-amount");
+  amountItem("increse-amount");
 }
 
+// total of 1 product
+function totalPricePrd(amount, index) {
+  let priceTotal = (amount * (document.querySelectorAll(".prd-price")[index].textContent.slice(1))).toFixed(2);
+  document.querySelectorAll(".prd-total")[index].innerHTML = "$"+priceTotal;
+}
+
+//change quality in cart when increase or decrease quality by input
+function changeQualityInput(value) {
+  let changeQuality = document.querySelectorAll('.amount-now');
+  let confirmValue = value < 1 ? 1 : value;
+  for(let i= 0; i < changeQuality.length; i++ ){
+    changeQuality[i].addEventListener("change", function() {
+      updateAmount(+confirmValue, i);
+      document.querySelectorAll(".amount-now")[i].value = confirmValue;
+      totalPricePrd(confirmValue, i)
+    })
+  }
+}
+
+//change quality in cart when increase or decrease quality by button
 function amountItem(action) {
   let changeAmount = document.querySelectorAll(`.${action}`);
   for (let i = 0; i < changeAmount.length; i++) {
     changeAmount[i].addEventListener("click", function () {
       let newAmount = Number(
-        document.querySelectorAll(".amount-now")[i].textContent
+        document.querySelectorAll(".amount-now")[i].value
       );
       if (action === "increse-amount") newAmount = newAmount + 1;
       if (action === "decrese-amount" && newAmount > 1) {
@@ -200,16 +124,17 @@ function amountItem(action) {
         document.querySelectorAll(".decrese-amount")[i].style.backgroundColor =
           "#fff";
       }
-      document.querySelectorAll(".amount-now")[i].innerHTML = newAmount;
-      updateCart(newAmount, i);
+      document.querySelectorAll(".amount-now")[i].value = newAmount;
+      totalPricePrd(newAmount, i);
+      updateAmount(newAmount, i);
       addNumberCart();
     });
   }
 }
-amountItem("decrese-amount");
-amountItem("increse-amount");
 
-function updateCart(amount, index) {
+
+// update quality product in localstorage
+function updateAmount(amount, index) {
   cartList = getItem();
   for (let i = 0; i < cartList.length; i++) {
     if (cartList[i] === cartList[index]) {
@@ -221,23 +146,21 @@ function updateCart(amount, index) {
   addNumberCart();
 }
 
-function removeItem() {
+// remove product in cart and set to localstorage
+function removeItems() {
   cartList = getItem();
   let remove = document.querySelectorAll(".remove");
   for (let i = 0; i < remove.length; i++) {
     if (cartList.length === 0) {
-      document.querySelector(".prd-list").innerHTML = "Giỏ hàng rỗng";
+      let empty = document.querySelector(".section-body");
+      empty.classList.add("empty");
+      empty.innerHTML = "Giỏ hàng trống";
       document.querySelector(".cart-body-right").innerHTML = "";
     }
     remove[i].addEventListener("click", function () {
-      console.log(i);
       let removePrd = cartList.slice(0, i).concat(cartList.slice(i + 1));
       localStorage.setItem("listItem", JSON.stringify(removePrd));
       displayCart();
-      updateCart();
-      removeItem();
     });
   }
 }
-updateCart();
-removeItem();
